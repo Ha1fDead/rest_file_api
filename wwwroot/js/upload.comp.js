746748d7005df370a3429611ce7be0be4391272c
Nathan Lafferty <nslafferty@gmail.com>
2019-04-10 21:15:54 -0500

    added some drag/drop to the file control

--- wwwroot/js/upload.comp.js
+++ wwwroot/js/upload.comp.js
@@ -1,9 +1,12 @@
 let template = document.createElement("template");
 template.innerHTML = `
-    <div>
-        <label for="upload">Upload a File</label>
-        <input type="file" id="upload">
-    </div>
+    <form id="uploadform">
+        <fieldset id="uploaddrop">
+            <label for="upload">Upload a File</label>
+            <input type="file" id="file">
+        </fieldset>
+        <button type="submit">Upload File</button>
+    </form>
 `;
 
 
@@ -13,6 +16,38 @@ export default class UploadComponent extends HTMLElement {
 
         const shadow = this.attachShadow({ mode: "open"});
         shadow.appendChild(template.content.cloneNode(true));
+
+        let dragdrop = shadow.getElementById("uploaddrop");
+        dragdrop.addEventListener("dragennter", this.handleDragEnter.bind(this), false);
+        dragdrop.addEventListener("dragover", this.handleDragOver.bind(this), false);
+        dragdrop.addEventListener("drop", this.handleDrop.bind(this), false);
+
+        let uploadform = shadow.getElementById("uploadform");
+        uploadform.addEventListener("submit", this.handleUserUpload.bind(this), false);
+    }
+
+    handleDragEnter(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    handleDragOver(event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    handleDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        const files = event.dataTransfer.files;
+        let filecontrol = this.shadowRoot.getElementById("file");
+        filecontrol.files = files;
+    }
+
+    handleUserUpload(event) {
+        event.preventDefault();
+        return false;
     }
 }
 
