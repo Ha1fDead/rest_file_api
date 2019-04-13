0ab37d343de77dcffccc1736d767af60ada59cb8
Nathan Lafferty <nslafferty@gmail.com>
2019-04-13 12:20:54 -0500

    added configuration for ifileprivder exclusions

--- Program.cs
+++ Program.cs
@@ -14,7 +14,18 @@ namespace maplarge_restapicore
     {
         private static Dictionary<string, string> appConfig = new Dictionary<string, string>()
         {
-            {"root_server_directory", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}
+            // configure where the root directory of the File Server is pointing at
+            // Files above this directory will be inaccessible
+            {"root_server_directory", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)},
+
+            // Following params are for the PhysicalFileProvider settings "Exclude Options"
+            // https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.fileproviders.physical.exclusionfilters?view=aspnetcore-2.2
+            // If true, hidden files will be affected and shown by the API
+            {"show_hidden", "false"},
+            // If true, files starting with a dot (".") will be shown
+            {"show_dot", "false"},
+            // If true, files set by the system will be shown
+            {"show_system", "false"},
         };
         public static void Main(string[] args)
         {
--- Startup.cs
+++ Startup.cs
@@ -9,11 +9,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
 
 namespace maplarge_restapicore
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
             if (!Directory.Exists(config["root_server_directory"]))
@@ -21,13 +24,33 @@ namespace maplarge_restapicore
                 Console.WriteLine("Configuration for root server public path invalid -- ensure path is valid and re-run program");
                 Environment.Exit(1);
             }
+
+            _config = config;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var exclusions = ExclusionFilters.None;
+            if (!Boolean.Parse(_config["show_hidden"]))
+            {
+                exclusions |= ExclusionFilters.Hidden;
+            }
+            if (!Boolean.Parse(_config["show_dot"]))
+            {
+                exclusions |= ExclusionFilters.DotPrefixed;
+            }
+            if (!Boolean.Parse(_config["show_system"]))
+            {
+                exclusions |= ExclusionFilters.System;
+            }
+
             services.AddResponseCompression();
             services.AddMvc();
+            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(
+                _config["root_server_directory"],
+                exclusions
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
