977b7da9866e6f311d9618ae1f4c25ae3c755407
Nathan Lafferty <nslafferty@gmail.com>
2019-04-18 15:13:43 -0500

    made pathmap more "functional"

--- rest_file_api/wwwroot/js/main.js
+++ rest_file_api/wwwroot/js/main.js
@@ -2,3 +2,15 @@ import DirectoriesComponent from './directories.comp.js';
 import UploadComponent from './upload.comp.js';
 import PathMapComponent from './pathmap.comp.js';
 import NotFoundComponent from './notfound.comp.js';
+import FileService from './file.service.js';
+
+
+window.addEventListener('popstate', event => {
+    let fs = new FileService();
+    let pathmaps = document.getElementsByTagName("demo-path-map");
+
+    for (const path of pathmaps) {
+        path.path = fs.GetCurrentRelativePath();
+    }
+
+}, false);
--- rest_file_api/wwwroot/js/pathmap.comp.js
+++ rest_file_api/wwwroot/js/pathmap.comp.js
@@ -1,4 +1,4 @@
-import {html, render} from 'https://unpkg.com/lit-html?module';
+import { html, render } from 'https://unpkg.com/lit-html?module';
 import FileService from './file.service.js';
 
 
@@ -12,6 +12,9 @@ style.textContent = `
 
 `;
 
+/**
+ * Renders the path with links to every part of the path
+ */
 export default class PathMapComponent extends HTMLElement {
 
 
@@ -20,22 +23,61 @@ export default class PathMapComponent extends HTMLElement {
 
         this.fileService = new FileService();
 
-        const shadow = this.attachShadow({mode: "open"});
+        const shadow = this.attachShadow({ mode: "open" });
         shadow.appendChild(style.cloneNode(true));
 
         this._wrapper = template.content.cloneNode(true);
         shadow.appendChild(this._wrapper);
-        this._update();
-        window.addEventListener('popstate', event => {
-            // Known limitation -- if user manually enters a URL we cannot intercept that
-            // Apparently not even Angular does it, which is fascinating!
-            this._update();
-        }, false);
     }
 
-    async _update() {
-        let directory = await this.fileService.GetDirectory(this.fileService.GetCurrentRelativePath());
+    static get observedAttributes() {
+        return ["path"];
+    }
 
+    /**
+     * Setter for the "Path" that this component is linking
+     * 
+     * @param {string} value
+     */
+    set path(value) {
+        this.setAttribute("path", value);
+    }
+
+    /**
+     * Getter for the "Path" that this component is linking
+     */
+    get path() {
+        return this.getAttribute("path");
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case "path":
+                this._update(this.path);
+                break;
+        }
+    }
+
+	connectedCallback() {
+		this.UpgradeProperties();
+    }
+
+    UpgradeProperties() {
+        for (const prop of PathMapComponent.observedAttributes) {
+            this.UpgradeProperty(prop);
+        }
+    }
+
+    UpgradeProperty(prop) {
+        if (this.hasOwnProperty(prop)) {
+            const value = this[prop];
+            delete this[prop];
+            this[prop] = value;
+        }
+        // console.log('set path', this.path);
+    }
+
+    async _update(path) {
         // need to split the string but return the entire string from the split
         let pathTemplate = (currentPath) => html`
             ${this.getPaths(currentPath).map((subpath) => html`
@@ -43,7 +85,7 @@ export default class PathMapComponent extends HTMLElement {
             `)}
         `;
 
-        render(pathTemplate(directory.relativePath), this.shadowRoot.getElementById("wrapper"));
+        render(pathTemplate(path), this.shadowRoot.getElementById("wrapper"));
     }
 
     /**
@@ -61,7 +103,7 @@ export default class PathMapComponent extends HTMLElement {
 
     getPaths(path) {
         let split = this.splitStringIntoParts(path, "/");
-        split.splice(0, 0, {previousPath: "/", part: "home"});
+        split.splice(0, 0, { previousPath: "/", part: "home" });
         return split;
     }
 
