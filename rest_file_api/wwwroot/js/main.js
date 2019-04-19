3813b2f79804ad3ff04a2af5b5475287a83bfc8f
Nathan Lafferty <nslafferty@gmail.com>
2019-04-19 10:38:24 -0500

    added proper unknown directory behavior

--- rest_file_api/wwwroot/js/main.js
+++ rest_file_api/wwwroot/js/main.js
@@ -3,6 +3,7 @@ import UploadComponent from './upload.comp.js';
 import PathMapComponent from './pathmap.comp.js';
 import NotFoundComponent from './notfound.comp.js';
 import FileService from './file.service.js';
+import FileComponent from './file.comp.js';
 
 
 
@@ -20,14 +21,24 @@ class Main {
             path.path = fs.GetCurrentRelativePath();
         }
     
-        let directory = await fs.GetDirectory(fs.GetCurrentRelativePath());
-        let directorycomp = document.getElementsByTagName("demo-directories");
-        for (const dir of directorycomp) {
-            dir.directory = directory;
+        let directorycomp = document.getElementsByTagName("demo-directories")[0];
+        let notfoundcomponent = document.getElementsByTagName("demo-not-found")[0];
+
+        try {
+            let directory = await fs.GetDirectory(fs.GetCurrentRelativePath())
+            directorycomp.style.display = "";
+            notfoundcomponent.style.display = "none";
+            directorycomp.directory = directory;
+        }
+        catch(e) {
+            // 404 or some other error
+            // Error message could really be improved here
+            notfoundcomponent.style.display = "";
+            directorycomp.style.display = "none";
         }
     }
 }
 
 const app = new Main();
-// popstate does not always get called from all browsers
+// popstate does not always get called from all browsers on init
 app.UpdateState();
